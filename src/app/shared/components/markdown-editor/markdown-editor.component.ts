0431abb70618560e9231d5f645cb5b55d53444d3
hellogithub2014 <1197128237@qq.com>
2017-07-25 23:18:54 +0800

    样式修改。  TODO：使用bootstrap样式、编辑器路由守卫

--- src/app/shared/components/markdown-editor/markdown-editor.component.ts
+++ src/app/shared/components/markdown-editor/markdown-editor.component.ts
@@ -17,21 +17,13 @@ import { EditorComponent } from './editor/editor.component';
   templateUrl: './markdown-editor.component.html',
   styleUrls: ['./markdown-editor.component.css']
 })
-export class MarkdownEditorComponent implements OnInit, AfterViewInit {
-
-  @ViewChildren(EditorComponent)
-  items: QueryList<EditorComponent>;
-
+export class MarkdownEditorComponent implements OnInit {
   @Output() artcileChange: EventEmitter<{ title: string, content: string }> = new EventEmitter();
 
-  ngAfterViewInit() {
-    console.log(this.items);
-  }
   constructor(public toastr: ToastsManager, vcr: ViewContainerRef) {
     this.toastr.setRootViewContainerRef(vcr);
   }
   ngOnInit() {
-    console.log(this.items);
     this.style = {
       'height.px': window.innerHeight
       // 'height.px': 500,
@@ -42,7 +34,7 @@ export class MarkdownEditorComponent implements OnInit, AfterViewInit {
     this.toastr.success('保存成功！', '系统提示')
       .then(_ => {
         this.artcileChange.emit({ title: event.title, content: event.value });
-      })
+      });
   }
 
   // tslint:disable-next-line:member-ordering
